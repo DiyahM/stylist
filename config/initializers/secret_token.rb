1aa4fe750ae4be55ccd89a59a5c563698abbbd51
Diyah <me@hadiyah.me>
2013-03-02 13:39:11 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stylist::Application.config.secret_token = 'e87ed39f26fecd97c66bd87d6b19d81009715d1f2803342c93680431b73b64817572b7ab617509472c5c78c39c91ec57ae8d5bf12d81459cbeb821a0dc8bfd0b'
