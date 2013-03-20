e20b5078e57ded16d0f455955b4e2d8f0d4172fe
Diyah <me@hadiyah.me>
2013-03-19 17:16:03 -0700

    add close button and highlight selection

--- public/image_selector.js
+++ public/image_selector.js
@@ -1,18 +1,26 @@
 var images  = jQuery('img');
 var large_images = [];
+var pattern = new RegExp("^(http|https)");
 for (var i=0;i<images.length;i++){
   if ((images[i].width > 200) && (images[i].height > 100))
-    large_images.push(images[i].src);
+    if (pattern.test(images[i].src)) 
+      large_images.push(images[i].src);
 }
 
-console.log(large_images);
+window.onmessage = function(event) {
+  if (event.data === "closed") {
+    location.reload();
+  }
+};
 var images_param = JSON.stringify(large_images);
-console.log("results " + large_images.toString());
 modal = document.createElement('iframe');
 modal.src = "http://local.host:3000/pages/image_picker?images=" + images_param;
-modal.setAttribute('style', 'border:3px solid;height:650px;position:fixed;left:25%;top:20px;width:50%;z-index:2000;'); 
-modal.className = 'modal';
+modal.setAttribute('style', 'border:3px solid;height:650px;position:fixed;left:25%;top:50px;width:50%;z-index:2000;'); 
+modal.id= 'modal';
 document.body.appendChild(modal);
+
+
+
 
 //window.open("http://local.host:3000/pages/image_picker","newwim","fullscreen=no,toolbar=no,status=no,resizable=no,width=350,height=550");
 
